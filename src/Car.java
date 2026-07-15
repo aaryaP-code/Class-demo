fd7676a3fcf6526b3141c2780001a1db2d65b9a2
Aarya-web-app <aarya19pandit@gmail.com>
2026-07-15 19:48:47 +0530

    Using Setter method.

--- src/Car.java
+++ src/Car.java
@@ -27,7 +27,23 @@ public class Car {
     }
 
     public void setMake(String make) {
-        make = make;
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 
     public void describeCar() {
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
+        car.setMake("Porsche");
+        car.setDoors(4);
+        car.setModel("Model Z");
+        car.setColor("Pink");
+        car.setConvertible(false);
 
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
